e7a0f78d915898abf996c79900c4d72040198898
Valentin <vainferro@gmail.com>
2023-02-22 23:58:27 +0300

    Created basic structure.

---
+++ script.js
@@ -0,0 +1,43 @@
+window.addEventListener('load', ()=>{
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 800
+    canvas.height = 720
+
+    class InputHandler {
+        constructor() {
+            this.keys = [];
+            window.addEventListener('keydown', (e)=>{
+                console.log(e)
+            })
+        }
+
+    }
+
+    class Player {
+
+    }
+
+    class Background {
+
+    }
+
+    class Enemy {
+
+    }
+
+    function handleEnemies() {
+
+    }
+
+    function displayStatusText() {
+
+    }
+
+    const input = new InputHandler();
+
+    function animate() {
+
+    }
+
+})
