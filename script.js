d15989df625529730ab2191b8a62c03f1a4d7d92
Valentin <vainferro@gmail.com>
2023-02-23 23:33:13 +0300

    Added inputs to class.

--- script.js
+++ script.js
@@ -7,15 +7,39 @@ window.addEventListener('load', ()=>{
     class InputHandler {
         constructor() {
             this.keys = [];
-            window.addEventListener('keydown', (e)=>{
-                console.log(e)
+            window.addEventListener('keydown', e => {
+                if (e.key === 'ArrowDown' ||
+                    e.key === 'ArrowUp' ||
+                    e.key === 'ArrowLeft' || 
+                    e.key === 'ArrowRight' && this.keys.indexOf(e.key) === -1) {
+                    this.keys.push(e.key);
+                }
+                
+            })
+            window.addEventListener('keyup', e => {
+                if (e.key === 'ArrowDown' ||
+                    e.key === 'ArrowUp' ||
+                    e.key === 'ArrowLeft' || 
+                    e.key === 'ArrowRight' && this.keys.indexOf(e.key) === 1) {
+                    this.keys.spice(e.key, this.keys);
+                    }
             })
         }
 
     }
 
     class Player {
-
+        constructor(gameWidth, gameHeight) {
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.width = 200;
+            this.height = 200;
+            this.x = 0;
+            this.y = 0;
+        }
+        draw(context) {
+            context.fillRect(this.x, this.y, this.width, this.height)
+        }
     }
 
     class Background {
